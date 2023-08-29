4750fcfac6a70b77bad493a647de6eceaae567a5
TrierRP <trier.rp.iceg@gmail.com>
2023-08-29 20:34:44 +0200

    123

---
+++ Addons/[Volkswagen]/[Golf 7 GTI]/golf7gti/fxmanifest.lua
@@ -0,0 +1,26 @@
+fx_version 'bodacious'
+game 'gta5'
+
+ -- Head over to https://docs.fivem.net/game-references/data-files/ for the data files
+ -- REMEMBER TO REMOVE THE FILE FROM files and data_file IF NOT IN RESOURCE!!
+ 
+files {
+    'vehicles.meta',
+    'carvariations.meta',
+    'carcols.meta',
+    'handling.meta',
+    'vehiclelayouts.meta',
+    'audioconfig/ea888_game.dat151.rel',
+    'audioconfig/ea888_sounds.dat54.rel',
+    'sfx/dlc_ea888/ea888.awc',
+    'sfx/dlc_ea888/ea888_npc.awc'
+}
+
+data_file 'HANDLING_FILE' 'handling.meta'
+data_file 'VEHICLE_METADATA_FILE' 'vehicles.meta'
+data_file 'CARCOLS_FILE' 'carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'carvariations.meta'
+data_file 'VEHICLE_LAYOUTS_FILE' 'vehiclelayouts.meta'
+data_file 'AUDIO_GAMEDATA' 'audioconfig/ea888_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/ea888_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_ea888'
